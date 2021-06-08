6b54c61235e7c62d7429089046e495f3cf85c5bd
Jackmanpig <Jackmanpig111@gmail.com>
2021-06-08 19:24:59 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -62,7 +62,7 @@ addLayer("e", {
     symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    requires: new Decimal(7), // Can be a function that takes requirement increases into account
     resource: "Energy", // Name of prestige currency
     baseResource: "Tree Power", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
