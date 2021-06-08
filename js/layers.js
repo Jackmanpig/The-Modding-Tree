034f6e4a57a82785877b1152b2b587191b36d1c5
Jackmanpig <Jackmanpig111@gmail.com>
2021-06-08 14:29:39 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -20,6 +20,7 @@ addLayer("t", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
+    canBuyMax() { return hasUpgrade("t", 11) },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "t", description: "T: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
