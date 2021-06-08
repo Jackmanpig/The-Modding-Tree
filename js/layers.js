4afe59f895463b5744a21c045f6eab9e0376c47a
Jackmanpig <Jackmanpig111@gmail.com>
2021-06-08 19:38:25 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -57,30 +57,3 @@ addLayer("t", {
     },
 
 })
-addLayer("e", {
-    name: "Energy", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    color: "#4BDC13",
-    requires: new Decimal(7), // Can be a function that takes requirement increases into account
-    resource: "Energy", // Name of prestige currency
-    baseResource: "Tree Power", // Name of resource prestige is based on
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.8, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
-    row: 1, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "e", description: "G: Reset for Energy", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){ return hasUpgrade("t", 14) },
-    upgrades: {
-   
-    },
-
-})
